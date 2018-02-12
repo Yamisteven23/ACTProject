cd4abbedfaff981ac98a45802c8461ded51446dd
Yamisteven23 <pet12058@byui.edu>
2018-02-12 10:57:06 -0700

    2/12/18 10:48 AM Adding read class

---
+++ ACTProject/src/actproject/Read.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actproject;
+
+import java.io.File;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author Yamisteven23
+ */
+public class Read {
+    public static void main(String[] args)
+    {
+        PrintStream out = System.out;
+        File file = new File("");
+        out.println("Open Successful");
+    }
+}
